0291d295d127636162192b20e734ae5bd5006aac
muthyam <dadigemuthyam@gmail.com>
2022-12-17 18:33:47 +0530

    g

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -32,10 +32,6 @@ public class Vehicle {
         System.out.println("stop method called - The vehicle is stopped");
     }
 
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-
     public int getCurrentDirection() {
         return currentDirection;
     }
@@ -44,15 +40,19 @@ public class Vehicle {
         this.currentDirection = currentDirection;
     }
 
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setCurrentSpeed(int currentSpeed) {
-        this.currentSpeed = currentSpeed;
     }
 }
