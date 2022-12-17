ab3775f0f4e467d020b15eecd889098a09b4375b
muthyam <dadigemuthyam@gmail.com>
2022-12-17 18:38:49 +0530

    g

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,6 +3,10 @@ package com.driver;
 public class Boat implements WaterVehicle{
     public String name;
     public int capacity;
+
+    public Boat() {
+    }
+
     @Override
     public String getVehicleName() {
         return name;
